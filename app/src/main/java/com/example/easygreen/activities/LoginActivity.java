d174b2c8a6dbbb3bc26c0d8a695d3ec9dd35e014
Gustavo Adame Delarosa <gua222@fb.com>
2020-07-09 00:44:05 -0400

    [Canary] Finished basic login with Facebook: There is a major bug with managing sessions for a user

--- app/src/main/java/com/example/easygreen/activities/LoginActivity.java
+++ app/src/main/java/com/example/easygreen/activities/LoginActivity.java
@@ -1,16 +1,85 @@
 package com.example.easygreen.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.easygreen.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.LoginStatusCallback;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * TODO: Figure out the best time to logout - LoginManager.getInstance().logOut();
+     */
+
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
+    private static final String EMAIL = "email";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton = findViewById(R.id.btnFBLogin);
+
+        /* Handle login responses */
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {}
+
+                    @Override
+                    public void onCancel() {}
+
+                    @Override
+                    public void onError(FacebookException exception) {}
+                });
+
+        LoginManager.getInstance().retrieveLoginStatus(this, new LoginStatusCallback() {
+            @Override
+            public void onCompleted(AccessToken accessToken) {}
+            @Override
+            public void onFailure() {}
+            @Override
+            public void onError(Exception exception) {}
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void openFB(View view) {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+        if (isLoggedIn){
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        } else {
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }
+    }
+
+    public void startApp(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
