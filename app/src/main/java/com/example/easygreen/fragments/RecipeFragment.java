3d1b09ebfba2b511f6d375882908983a099114c5
Gustavo Adame Delarosa <gua222@fb.com>
2020-07-12 23:11:18 -0400

    [Canary] Added key views for Recipe Screen: Have to create an adapter for recipes

--- app/src/main/java/com/example/easygreen/fragments/RecipeFragment.java
+++ app/src/main/java/com/example/easygreen/fragments/RecipeFragment.java
@@ -1,26 +1,32 @@
 package com.example.easygreen.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import com.example.easygreen.R;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
 public class RecipeFragment extends Fragment {
-
-    public RecipeFragment() {}
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private ChipGroup cgFilterContainer;
+    private Chip chip1, chip2, chip3;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_recipe, container, false);
+        View view = inflater.inflate(R.layout.fragment_recipe, container, false);
+
+        inflateViews(view);
+        return view;
+    }
+
+    private void inflateViews(View view) {
+        cgFilterContainer = view.findViewById(R.id.cgFilterContainer);
+        chip1 = view.findViewById(R.id.chip1);
+        chip2 = view.findViewById(R.id.chip2);
+        chip3 = view.findViewById(R.id.chip3);
     }
 }
