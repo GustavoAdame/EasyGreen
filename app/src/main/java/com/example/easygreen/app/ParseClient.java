bc34294118922728c6e2cd1abae453e885058e66
Gustavo Adame Delarosa <gua222@fb.com>
2020-07-21 13:18:54 -0400

    [BETA] Refactor: Added comments to ParseClient

--- app/src/main/java/com/example/easygreen/app/ParseClient.java
+++ app/src/main/java/com/example/easygreen/app/ParseClient.java
@@ -11,9 +11,11 @@ public class ParseClient extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        /***** Use in-App data models as ParseObjects *********/
         ParseObject.registerSubclass(Inventory.class);
         ParseObject.registerSubclass(ShoppingList.class);
 
+        /***** Connect to Server and Backend *********/
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("easygreen")
                 .server("https://easygreen.herokuapp.com/parse/")
